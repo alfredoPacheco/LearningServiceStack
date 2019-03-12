c83ab4f6a4770f62ff2f1194affbcd04e8f4dea6
alfredoPacheco <j.alfredo.pacheco@gmail.com>
2019-03-12 17:56:02 -0600

    Entity Framework

---
+++ MyApp.EF/EfContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MyApp.EF
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<UserGreeting> UserGreetings { get; set; }
+    }
+}
---
+++ MyApp.EF/UserGreeting.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApp.EF
+{
+    public class UserGreeting
+    {
+        public int Id { get; set; }
+
+        public string Greeting { get; set; }
+    }
+}
